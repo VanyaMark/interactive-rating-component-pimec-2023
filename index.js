fa109f8221d0332ef578cbcdda2e9ca7639519ea
VanyaMark <markova.vanya@gmail.com>
2024-05-13 21:26:22 +0200

    done with forEach

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let ratingInput = 0;
 
 function clickNumber(rating) {
     ratingInput = rating;
-    console.log('numberInput: ', ratingInput);
+//    console.log('numberInput: ', ratingInput);
     submit.style.backgroundColor ='hsl(25 97% 53%)';
     submit.removeAttribute('disabled');
 }
@@ -23,11 +23,15 @@ function clickSubmit() {
 
 }
 
-ratingsArray[0].addEventListener('click', () => clickNumber(ratingsArray[0].dataset.rating));
-ratingsArray[1].addEventListener('click', () => clickNumber(ratingsArray[1].dataset.rating));
-ratingsArray[2].addEventListener('click', () => clickNumber(ratingsArray[2].dataset.rating));
-ratingsArray[3].addEventListener('click', () => clickNumber(ratingsArray[3].dataset.rating));
-ratingsArray[4].addEventListener('click', () => clickNumber(ratingsArray[4].dataset.rating));
+// ratingsArray[0].addEventListener('click', () => clickNumber(ratingsArray[0].dataset.rating));
+// ratingsArray[1].addEventListener('click', () => clickNumber(ratingsArray[1].dataset.rating));
+// ratingsArray[2].addEventListener('click', () => clickNumber(ratingsArray[2].dataset.rating));
+// ratingsArray[3].addEventListener('click', () => clickNumber(ratingsArray[3].dataset.rating));
+// ratingsArray[4].addEventListener('click', () => clickNumber(ratingsArray[4].dataset.rating));
+
+ratingsArray.forEach(elem => {
+    elem.addEventListener('click', (event) => {clickNumber(event.target.dataset.rating)});
+});
 
 submit.addEventListener('click', clickSubmit)
 
